6afeaabf5bc58ae786a20e821bff99d4206afa80
frank-bless <franktsetse98@gmail.com>
2024-12-18 23:00:13 -0800

    Done with the assigment

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,48 @@
 public class Book {
-
-    // This is your working class.
-    
+    // Private attributes
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+    // Constructor
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = true; // Books are available when created
+    }
+    // Getters and setters for encapsulation
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
 }
